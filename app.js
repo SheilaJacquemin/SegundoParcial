b1bc2135f4273628013ad4338d12da89ea7bd4f6
SheilaJacquemin <sheilajacquemin@gmail.com>
2023-06-21 12:31:54 -0300

    Sheila-Jacquemin-21/06/2023-

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
 const express = require('express');
-const morgan = require('morgan')
 const cors = require('cors');
+const morgan = require('morgan');
+
+const productRoutes = require('../src/routes/product.routes');
+
 const app = express();
+const port = 3000;
 
-const { query } = require('./database');
-
-const port = process.env.PORT
+// MIDDLEWARE
+app.use(cors())
+app.use(morgan("combined"))
 
 app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'))
+app.use('/productos', productRoutes);
 
-// Ruta GET para obtener productos
-app.get('/productos', async (req, res) => {
-  try {
-    const rows = await query('SELECT * FROM products');
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al obtener los productos' });
-  }
-});
-
-// Ruta POST para subir un producto
-app.post('/productos', async (req, res) => {
-  const { name, price } = req.body;
-  const values = [name, price];
-
-  try {
-    await query('INSERT INTO products (name, price) VALUES (?, ?)', values);
-    res.status(201).json({ message: 'Producto subido exitosamente' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al subir el producto' });
-  }
-});
-
-app.listen(4000, () => {
-  console.log('El servidor esta corriendo puerto 4000');
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
--- dockerfile
+++ dockerfile
@@ -1,13 +1,22 @@
-FROM node:latest
+# Define la imagen base
+FROM node:18-alpine
 
-WORKDIR /app
+# Establece el directorio de trabajo dentro del contenedor
+WORKDIR /usr/src/dockerserver
 
-COPY package.json .
+# Copia los archivos de la aplicación al directorio de trabajo del contenedor
+COPY package*.json ./
 
-RUN npm i
+# Instala las dependencias de la aplicación
+RUN npm install
 
+# Copia el resto de los archivos de la aplicación
 COPY . .
 
-EXPOSE 4500
+# Expone el puerto en el que el contenedor escuchará las solicitudes
+EXPOSE 3000
 
+# Comando para ejecutar la aplicación cuando se inicie el contenedor
+# CMD ["npm", "start"]
 CMD ["npm", "run", "dev"]
+# CMD ["node", "server.js"]
